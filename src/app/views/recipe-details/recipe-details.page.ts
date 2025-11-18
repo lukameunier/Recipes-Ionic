5fe70829078f21c8e2333723b57d7e9db134e5be
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-11-18 02:21:58 +0100

    Add the logic for the ingredient's ratio

--- src/app/views/recipe-details/recipe-details.page.ts
+++ src/app/views/recipe-details/recipe-details.page.ts
@@ -1,34 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonRow, IonButton, IonLabel, IonImg, IonList, IonIcon, IonCard } from '@ionic/angular/standalone';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonCol, IonRow, IonButton, IonLabel, IonImg, 
+  IonList, IonIcon, IonCard
+} from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
-import { RouterLink } from '@angular/router';
 import { RecipesRepository } from 'src/app/repository/recipes-repository';
 import { Recipe } from 'src/app/data/recipe';
+import { Ingredient } from 'src/app/data/ingredient';
+import { RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { add, remove } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonRow, IonButton, IonLabel, IonImg, IonList, IonIcon, IonCard, RouterLink],
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    CommonModule, FormsModule, IonCol, IonRow,
+    IonButton, IonLabel, IonImg, IonList, IonIcon,
+    RouterLink, IonCard
+  ],
 })
 export class RecipeDetailsPage implements OnInit {
 
   recipe: Recipe | undefined = undefined;
 
-  constructor(private recipesRepository: RecipesRepository, private route: ActivatedRoute) {
-    addIcons({ add, remove });
+  currentServings = 0;
+
+  constructor(
+    private recipesRepository: RecipesRepository,
+    private route: ActivatedRoute
+  ) {
+    addIcons({add, remove});
   }
 
   ngOnInit() {
     const name = this.route.snapshot.paramMap.get('name');
     if (name != null) {
       this.recipe = this.recipesRepository.getRecipe(name);
+      if (this.recipe) {
+        this.currentServings = this.recipe.servings;
+      }
     }
+  }
+
+  increaseServings() {
+    if (this.recipe) {
+      this.currentServings++;
+    }
+  }
+
+  decreaseServings() {
+    if (this.recipe && this.currentServings > 1) {
+      this.currentServings--;
+    }
+  }
+
+  getScaledQuantity(ingredient: Ingredient): number | undefined {
+    if (!this.recipe || ingredient.quantity == null) {
+      return ingredient.quantity;
+    }
+    const baseServings = this.recipe.servings;
+    return ingredient.quantity * this.currentServings / baseServings;
   }
 
   capitalizeFirst(str: string): string {
